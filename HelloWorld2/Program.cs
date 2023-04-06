491cecf9f445ca33ee5f943c7e311bc85fbf5c4a
Gihun Ham <ham@e-mirim.hs.kr>
2023-04-06 14:47:41 +0900

    출력 메시지를 한글로 수정 #1

--- HelloWorld2/Program.cs
+++ HelloWorld2/Program.cs
@@ -10,7 +10,7 @@ namespace HelloWorld2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("안녕 세상아~");
         }
     }
 }
